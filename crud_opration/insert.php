faefbc92595001b8aa9ef39774b0ff61ed7ff009
tanjil-hosain <tanjilh85@gmail.com>
2026-04-27 19:42:58 +0600

    insert

---
+++ crud_opration/insert.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        Name: <br> <br>
+        <input type="text" name="name"> <br> <br>
+        price : <br>
+         <br>
+         <input type="text" name="price"> <br><br>
+         <input type="submit" name="insert" value="ADD Product">
+    </form>
+</body>
+</html>
