f2e8a33f214c1e67a33133da8821da49504871fd
tanjil-hosain <tanjilh85@gmail.com>
2026-04-28 20:16:38 +0600

    product add form

--- gedget_shop/product_add.php
+++ gedget_shop/product_add.php
@@ -3,9 +3,50 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>BRAND ADD</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    hii
+
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+
+            <div class="card shadow p-4">
+                <h2 class="text-center mb-4">PRODUCT ADD</h2>
+
+                <form action="" method="POST">
+
+                    <div class="mb-3">
+                        <label class="form-label">Name</label>
+                        <input type="text" name="name" class="form-control" placeholder="Brand Name">
+                    </div>
+                    
+                    <div class="mb-3">
+                        <label class="form-label">Price</label>
+                        <input type="text" name="price" class="form-control" placeholder="Contact">
+                    </div>
+                                        <div class="mb-3">
+                        <label class="form-label">Brand_id</label>
+                        <input type="text" name="brand_id" class="form-control" placeholder="Contact">
+                    </div>
+                                        <div class="mb-3">
+                        <label class="form-label">Product</label>
+                        <input type="text" name="price" class="form-control" placeholder="Contact">
+                    </div>
+
+                    <button type="submit" name="brand" class="btn btn-primary w-100">
+                        Submit
+                    </button>
+
+                </form>
+            </div>
+
+        </div>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
