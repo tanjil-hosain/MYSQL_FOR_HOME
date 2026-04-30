ddc00e4c250ba6cdec6ac2654989fd4b93dd6e5f
tanjil-hosain <tanjilh85@gmail.com>
2026-04-30 10:33:26 +0600

    login

--- gedget_shop/login.php
+++ gedget_shop/login.php
@@ -1,11 +1,56 @@
+<?php
+$database = mysqli_connect("localhost", "root", "", "gedget_shop");
+if(isset($_POST['login'])){
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM users";
+
+    if(mysqli_query($database, $sql)){
+        header("location:view.php");
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Log In</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    hiii
+
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+
+            <div class="card shadow p-4">
+                <h2 class="text-center mb-4">Login</h2>
+
+                <form action="" method="POST">
+                    <div class="mb-3">
+                        <label class="form-label">Email</label>
+                        <input type="text" name="email" class="form-control" placeholder="Email">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Password</label>
+                        <input type="password" name="password" class="form-control" placeholder="Password">
+                    </div>
+
+
+                    <button type="submit" name="login" class="btn btn-primary w-100">
+                        login
+                    </button>
+
+                </form>
+            </div>
+
+        </div>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
